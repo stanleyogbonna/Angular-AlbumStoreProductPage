faaccc3083bab604dcda67b9ee81cba063810796
Stanley Ogbonna <ogbonnastanley@gmail.com>
2020-10-01 18:14:46 +0100

    product description component second updates

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
-import { map } from 'rxjs/add/operator/map'
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class ProductService {
@@ -8,8 +8,7 @@ export class ProductService {
   constructor(private _http: Http) { }
 
   getAlbum(id: number) {
-    return this._http.get(this._albumUrl)
-      .pipe(map((response: { json: any; }) => response.json))
+    return this._http.get(this._albumUrl).map((response: { json: () => any; }) => response.json());
   }
 
 }
